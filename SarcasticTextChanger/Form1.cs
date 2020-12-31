19a0991c4618dafd41a5924eac0729b96f656a5a
Xavier Baszuk <48223045+baszukxj@users.noreply.github.com>
2020-12-30 19:49:01 -0500

    Add functionality

--- SarcasticTextChanger/Form1.cs
+++ SarcasticTextChanger/Form1.cs
@@ -16,5 +16,52 @@ namespace SarcasticTextChanger
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Sarcastify(textBox1.Text);
+        }
+
+        private string Sarcastify(string str)
+        {
+            char[] arr = str.ToCharArray();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(arr[i].ToString()))
+                {
+                    continue;
+                }
+                if(i%2 == 0)
+                {
+                    arr[i] = char.ToUpper(arr[i]);
+                }
+                else
+                {
+                    arr[i] = char.ToLower(arr[i]);
+                }
+            }
+
+            return new string(arr);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(textBox2.Text);
+        }
     }
 }
